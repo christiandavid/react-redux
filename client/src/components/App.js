342fe03772f0f74b33cae3091d4eb0db834bb4c4
Christian David <christianibarguen@gmail.com>
2019-09-24 08:26:24 -0500

    Adds the initial structure for the app

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddText from '../container/AddText';
+import TextLists from '../container/TextLists';
+
+function App() {
+  return (
+    <div>
+      <AddText />
+      <TextLists />
+    </div>
+  );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import App from './components/App';
+import appReducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(appReducer, applyMiddleware(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
 serviceWorker.unregister();
